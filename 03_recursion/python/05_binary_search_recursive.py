d1bab2ffa0c829bcc7cad1251fa8c9e352307300
propeople8 <66854138+propeople8@users.noreply.github.com>
2022-02-15 21:50:22 +0800

    Update 05_binary_search_recursive.py

--- 03_recursion/python/05_binary_search_recursive.py
+++ 03_recursion/python/05_binary_search_recursive.py
@@ -10,6 +10,6 @@ def binary_search(arr, target):
     mid = (low + high) // 2
 
     if arr[mid] > target:
-        return binary_search(arr[:mid], target)
+        return binary_search(arr[:mid-1], target)
     else:
         return binary_search(arr[mid+1:], target)
